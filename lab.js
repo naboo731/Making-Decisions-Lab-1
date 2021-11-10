0d16872e817fb685e06603bc9d01fd20e4988a05
Nichole Boothman <nbooth731@gmail.com>
2021-11-10 18:08:33 -0500

    Problem 10 solved

--- lab.js
+++ lab.js
@@ -126,19 +126,20 @@ let passingScore = 7
 
 */
 
-let changeMyMind = true
-changeMyMind = !changeMyMind
-console.log(changeMyMind)
-changeMyMind = !changeMyMind
-console.log(changeMyMind)
+// let changeMyMind = true
+// changeMyMind = !changeMyMind
+// console.log(changeMyMind)
+// changeMyMind = !changeMyMind
+// console.log(changeMyMind)
 
 
-////////// PROBLEM 10 //////////
-// Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
+// ////////// PROBLEM 10 //////////
+// // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
-//CODE HERE
+// //CODE HERE
 
-
+// changeMyMind != changeMyMind
+// console.log(changeMyMind)
 
 ////////// ADVANCED PROBLEMS //////////
 
